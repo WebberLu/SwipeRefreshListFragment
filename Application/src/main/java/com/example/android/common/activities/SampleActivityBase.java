9b8d3585db4bed8ac9ca02a233f76006e46decac
WebberLu <e1e1lu32@gmail.com>
2015-07-24 13:57:22 +0800

    second commit

--- Application/src/main/java/com/example/android/common/activities/SampleActivityBase.java
+++ Application/src/main/java/com/example/android/common/activities/SampleActivityBase.java
@@ -29,6 +29,8 @@ public class SampleActivityBase extends FragmentActivity {
 
     public static final String TAG = "SampleActivityBase";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
